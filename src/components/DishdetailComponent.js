e6b7ea7cd0197973eb040fecd8c669b896a2dd44
Bibek Thapaliya <itsmebvek@gmail.com>
2018-10-10 12:32:29 +0200

    Fetch Post Comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -31,7 +31,7 @@ class CommentForm extends Component{
     }
     handleSubmitComment(values){
         this.toggleModal();
-       this.props.addComment(this.props.dishId,values.rating,values.author,values.comment);
+       this.props.postComment(this.props.dishId,values.rating,values.author,values.comment);
     }
 
 
@@ -100,7 +100,7 @@ class CommentForm extends Component{
 }
 
 
-     function  RenderComments({comments,addComment,dishId}){
+     function  RenderComments({comments,postComment,dishId}){
         if (comments==null){
             return (<div></div>)
         }
@@ -126,7 +126,7 @@ class CommentForm extends Component{
                 <ul className='list-unstyled'>
                     {cmmts}
                 </ul>
-                <CommentForm dishId={dishId} addComment={addComment} />
+                <CommentForm dishId={dishId} postComment={postComment} />
             </div>
         )
     }
@@ -185,7 +185,7 @@ class CommentForm extends Component{
                     </div>
                     <div className='row'>
                         <RenderDish dish={props.dish}/>
-                        <RenderComments comments={props.comments} addComment={props.addComment} dishId={props.dish.id}/>
+                        <RenderComments comments={props.comments} postComment={props.postComment} dishId={props.dish.id}/>
 
                     </div>
                 </div>
